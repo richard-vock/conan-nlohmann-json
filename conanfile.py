58c88593d110916b6b31b9ddc227b699e0b10022
Paul Hilbert <me@sicgraph.org>
2019-12-26 17:42:35 +0100

    initial commit

---
+++ conanfile.py
@@ -0,0 +1,23 @@
+from conans import ConanFile, CMake, tools
+
+
+class NLohmannJSONConan(ConanFile):
+    name = "nlohmann-json"
+    version = "3.7.3"
+    license = "LLVM"
+    author = "Richard Vock <vock@cs.uni-bonn.de>"
+    url = "https://github.com/richard-vock/conan-nlohmann-json"
+    description = "JSON for Modern C++ (https://nlohmann.github.io/json/)"
+    topics = ("json", "parser")
+    settings = "os"
+
+    def source(self):
+        git = tools.Git(folder='json')
+        git.clone("https://github.com/nlohmann/json.git", "v3.7.3")
+
+    def package(self):
+        self.copy("*.hpp", dst="include/json", src="json/include/nlohmann", keep_path=True)
+
+    def package_id(self):
+        self.info.header_only()
+
